608a6500f7e18992d649c359617f86b286846fc8
VincentTsun <104469464+VincentTsun@users.noreply.github.com>
2022-05-02 22:18:48 -0700

    modified bootstrap, bug fixes
    modified bootstrap to be locally referenced for customization, fixed several bugs

--- website/auth.py
+++ website/auth.py
@@ -18,7 +18,7 @@ def login():
             if check_password_hash(user.password, password):
                 flash('Logged in succecssfully!', category='success')
                 login_user(user, remember=True)
-                return redirect(url_for('views.posts'))
+                return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
--- website/static/index.js
+++ website/static/index.js
@@ -1,11 +1,3 @@
-function deletePost(postId) {
-    fetch("/delete-post", {
-        method: 'POST',
-        body: JSON.stringify({ postId: postId }),
-    }).then((_res) => {
-        window.location.href = "/";
-    });
-}
 
 (function () {
     'use strict'
